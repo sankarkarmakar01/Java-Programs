f80910e93105b32cdfe7f039117bb299446b617e
sankarkarmakar01 <karmakarsankar000@gmail.com>
2025-01-12 10:11:44 +0530

    Multi Threading

---
+++ multithreading/Test.java
@@ -0,0 +1,14 @@
+package multithreading;
+
+public class Test {
+
+    public static void main(String[] args) {
+        World world = new World();
+        world.start();
+
+        for (int i = 0; i <= 1000; i++) {
+            System.out.println(Thread.currentThread().getName());
+        }
+
+    }
+}
