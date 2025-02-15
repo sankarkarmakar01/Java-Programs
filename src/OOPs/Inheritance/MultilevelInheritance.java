c22c137b0982014ed0bd23058ebb08899a59a697
sankarkarmakar01 <karmakarsankar000@gmail.com>
2025-02-15 20:29:51 +0530

    Next

--- src/OOPs/Inheritance/MultilevelInheritance.java
+++ src/OOPs/Inheritance/MultilevelInheritance.java
@@ -1,9 +1,20 @@
 package OOPs.Inheritance;
 
-class calf extends Cow {
-
+class Calf extends Cow {
+    void hello3() {
+        System.out.println("I am a Calf");
+    }
 }
 
 public class MultilevelInheritance {
-
+    public static void main(String[] args) {
+        Animal a = new Animal();
+        Cow c = new Cow();
+        Calf ca = new Calf();
+        a.hello();
+        c.hello();
+        ca.hello();
+        ca.hello2();
+        ca.hello3();
+    }
 }
