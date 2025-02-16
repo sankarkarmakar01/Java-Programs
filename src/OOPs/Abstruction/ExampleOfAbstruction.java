c38e36a5cc5daca5efc278df248e35771bb16c3c
sankarkarmakar01 <karmakarsankar000@gmail.com>
2025-02-16 20:45:08 +0530

    Commit of 16-02-2025

---
+++ src/OOPs/Abstruction/ExampleOfAbstruction.java
@@ -0,0 +1,23 @@
+package OOPs.Abstruction;
+
+public class ExampleOfAbstruction {
+    public static void main(String[] args) {
+        Car c = new Car();
+        c.greed();
+        c.name();
+    }
+}
+
+abstract class Vehicle {
+    abstract void greed();
+    void name() {
+        System.out.println("Sankar Karmakar");
+    }
+}
+
+class Car extends Vehicle {
+    @Override
+    void greed() {
+        System.out.println("I am car");
+    }
+}
