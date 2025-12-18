31d0813a04ee0b89ae420739f939670450f4a0bc
sankarkarmakar01 <karmakarsankar000@gmail.com>
2025-12-18 12:39:02 +0530

    commit for java revision  basics

---
+++ src/javaRevision/baiscs/HelloWorld.java
@@ -0,0 +1,7 @@
+package javaRevision.baiscs;
+
+public class HelloWorld {
+    static void main(String[] args) {
+        System.out.println("Hello, Welcome to this entire cursed world of enemies...");
+    }
+}
